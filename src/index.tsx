7b05eba39c2ee5cd40b678c8f80a534546070a42
GuiCMartins <guilhermecmartins99@gmail.com>
2022-07-22 20:35:23 -0300

    Inicio da criacao dos Routers

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import SignUp from './pages/SignUp';
 import 'primeicons/primeicons.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   //<React.StrictMode>
-  <SignUp />
+
+  <Router>
+    <Routes>
+      <Route path="/" element={<SignUp />} />
+    </Routes>
+  </Router>
+
   //</React.StrictMode>
 );
