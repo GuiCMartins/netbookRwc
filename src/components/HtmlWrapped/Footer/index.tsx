0ff75fb47ecd7b13c96585326f34ef50fc76ae16
GuiCMartins <guilhermecmartins99@gmail.com>
2022-07-22 22:03:40 -0300

    Criacao do footer

---
+++ src/components/HtmlWrapped/Footer/index.tsx
@@ -0,0 +1,11 @@
+import { Copyright, FooterHtml } from './styles';
+
+const Footer = () => {
+  return (
+    <FooterHtml>
+      <Copyright>Copyright &copy; NetBook. All Rights Reserved</Copyright>
+    </FooterHtml>
+  );
+};
+
+export default Footer;
---
+++ src/components/HtmlWrapped/Footer/styles.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const FooterHtml = styled.footer`
+  width: 100vw;
+  height: 8vh;
+  background: #2a323d;
+  position: absolute;
+  bottom: 0;
+`;
+
+export const Copyright = styled.p`
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  font-size: 12px;
+`;
