1406914dd4bebc0413891404f50605f49c8ff0a4
GuiCMartins <guilhermecmartins99@gmail.com>
2022-07-23 00:14:46 -0300

    Adicionada a rota de acdastro no router

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import SignUp from './pages/SignUp';
 import 'primeicons/primeicons.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import InitialPageHeader from 'components/HtmlWrapped/InitialPageHeader';
+import Footer from 'components/HtmlWrapped/Footer';
+import SignIn from 'pages/SignIn';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +15,12 @@ root.render(
 
   <Router>
     <Routes>
-      <Route path="/" element={<SignUp />} />
+      <Route path="/" element={<InitialPageHeader />}>
+        <Route index element={<SignIn />} />
+      </Route>
+      <Route path="/cadastro" element={<SignUp />} />
     </Routes>
+    <Footer />
   </Router>
 
   //</React.StrictMode>
