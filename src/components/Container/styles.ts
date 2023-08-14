bc382c3e88ed187b148ea2170c6c0fe081e40031
GuiCMartins <guilhermecmartins99@gmail.com>
2023-08-14 20:15:11 -0300

    Ajuste no footer

--- src/components/Container/styles.ts
+++ src/components/Container/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
 
 export const PrimaryContainer = styled.div`
-  height: 80vh;
+  position: relative;
 `;
--- src/components/HtmlWrapped/Footer/index.tsx
+++ src/components/HtmlWrapped/Footer/index.tsx
@@ -2,7 +2,7 @@ import { Copyright, FooterHtml } from './styles';
 
 const Footer = () => {
   return (
-    <FooterHtml className="position-fixed vw-100 bottom-0">
+    <FooterHtml className="position-absolute vw-100">
       <Copyright className="d-flex justify-content-center align-items-center h-100">
         Copyright &copy; NetBook. All Rights Reserved
       </Copyright>
